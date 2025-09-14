b9fa54cf8288f419833ac959e4ebf396f9c54835
MIGUEL ALEJANDRO RODRIGUEZ HECHAVARRIA <103004569+benjammdoleyyy@users.noreply.github.com>
2025-09-14 17:49:06 -0400

    Update footer.py

--- portafolio/views/footer.py
+++ portafolio/views/footer.py
@@ -1,12 +1,29 @@
 import reflex as rx
-from portafolio.components.media import media
+from portafolio.components.icon_button import icon_button
 from portafolio.data import Media
 from portafolio.styles.styles import Size
 
 
 def footer(data: Media) -> rx.Component:
     return rx.vstack(
-        rx.text("Nombre"),
-        media(data),
-        spacing=Size.SMALL.value
+        rx.hstack(
+            icon_button(
+                "message-circle",
+                f"https://wa.me/5356933884",
+                "",
+                False
+            ),
+            spacing=Size.SMALL.value,
+            justify="center"
+        ),
+        rx.text(
+            "© 2025 Neisser.tech - Todos los derechos reservados",
+            text_align="center",
+            font_size="sm",
+            color="gray",
+            margin_top=Size.SMALL.value
+        ),
+        spacing=Size.SMALL.value,
+        align="center",
+        width="100%"
     )
